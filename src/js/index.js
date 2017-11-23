c83d90ba9589dc84b377ae22960b7aad8527e514
Jack N <jacknagle22@gmail.com>
2017-11-24 03:00:13 +1100

    reeeeee

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-$(".button-collapse").sideNav();
+$(document).ready(() => {
+    $(".button-collapse").sideNav({ menuWidth: 220 });
+    $(".parallax").parallax();
+    $(".modal").modal();
+});
+
+$("a.smooth-scroll[href*=\"#\"]:not([href=\"#\"])").click(function () {
+    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+        if (target.length) {
+            $("html, body").animate({ scrollTop: target.offset().top - 70 }, 1000, "easeInOutExpo");
+            return false;
+        }
+    }
+});
